f1bc665cafc3f9f70519c740d6808e2db1d5215c
Francisco Adrián Martínez Dengra <fmartinez3@ies-eugeni.cat>
2025-05-18 11:35:14 +0200

    ClasesComit

---
+++ Usuari.py
@@ -0,0 +1,14 @@
+class Usuari:
+    def __init__(self, nom: str = "None", cognoms: str = "None", dni: str = "None") -> str:
+        self.nom = nom
+        self.cognoms = cognoms
+        self.dni = dni
+
+    def imprimir_dades(self) -> str:
+        return f"DNI: {self.dni}, Nom: {self.nom}, Cognoms: {self.cognoms}"
+
+    def introduir_dades(self) -> str:
+        self.nom = input("Introdueix el nom de l'usuari: ")
+        self.cognoms = input("Introdueix els cognoms de l'usuari: ")
+        self.dni = input("Introdueix el DNI de l'usuari: ")
+        return "Dades de l'usuari introduïdes correctament."
